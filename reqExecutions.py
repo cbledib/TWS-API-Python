ac8d576c236fb3ff7c2bfcbd46869009c6aeb10c
cbledib <167079326+cbledib@users.noreply.github.com>
2025-10-09 13:40:05 +0100

    Create reqExecutions.py

---
+++ reqExecutions.py
@@ -0,0 +1,29 @@
+from ibapi.client import *
+from ibapi.wrapper import *
+
+port = 7497
+
+class TestApp(EClient, EWrapper):
+
+    def __init__(self):
+        EClient.__init__(self, self)
+
+    def nextValidId(self, orderId: OrderId):
+        exec_filter = ExecutionFilter()
+
+        self.reqExecutions(
+            orderId,
+            exec_filter
+        )
+
+    def execDetails(self, reqId: int, contract: Contract, execution: Execution):
+        print(reqId, contract, execution)
+
+    def execDetailsEnd(self, reqId: int):
+        print("execDetailsEnd.", reqId)
+        self.disconnect()
+
+
+app = TestApp()
+app.connect("127.0.0.1", port, 0)
+app.run()
